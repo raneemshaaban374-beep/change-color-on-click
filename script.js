72bfc1522856d7c7c760a7154fa2fb1fb345a370
raneemshaaban374-beep <raneemshaaban374@gmail.com>
2026-09-23 17:04:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("btn1").addEventListener("click",function(){
+    document.body.style.backgroundColor="red";
+});
+document.getElementById("btn2").addEventListener("click",function(){
+    document.body.style.backgroundColor="blue";
+});
+document.getElementById("btn3").addEventListener("click",function(){
+    document.body.style.backgroundColor="green";
+});
+document.getElementById("btn4").addEventListener("click",function(){
+    document.body.style.backgroundColor="white";
+});
